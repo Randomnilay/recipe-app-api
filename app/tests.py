95be76fb6eeac03dbcbe042fd4bcb7281d7ddc7f
Patel Nilay <101557733+Randomnilay@users.noreply.github.com>
2023-01-31 18:03:23 +0530

    test added

---
+++ app/tests.py
@@ -0,0 +1,29 @@
+"""sample test"""
+
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """tetst the calc modules"""
+
+    def test_add_numbers(self):
+        res = calc.add(5,6
+        )
+
+
+        self.assertEqual(res, 11)
+
+
+
+        
+
+
+    def test_subtracting_numbers(self):
+        """testing the subtraction of numbers"""
+        res = calc.subtract(15,10)
+
+
+        self.assertEqual(res, 5)
