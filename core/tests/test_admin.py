bfbd35bea11520500e4130b4ad741ccaf662c376
Patel Nilay <101557733+Randomnilay@users.noreply.github.com>
2023-02-02 16:44:59 +0530

    error resolved

--- core/tests/test_admin.py
+++ core/tests/test_admin.py
@@ -1,50 +1,42 @@
 """
-test for the Django admin notification
-
+Tests for the Django admin modifications.
 """
-
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django import Client
+from django.test import Client
 
 
-class AdminSideTests(TestCase):
-    """test for django admin"""
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
 
     def setUp(self):
-        """create user and client"""
+        """Create user and client."""
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_superuser (
-            email='user@example.com',
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
             password='testpass123',
-
         )
         self.client.force_login(self.admin_user)
-        self.admin_user = get_user_model().objects.create_user (
+        self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='test user'
-
+            name='Test User'
         )
 
-    def test_user_list(self):
-        """test that useer are listed on page"""
+    def test_users_lists(self):
+        """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-
     def test_edit_user_page(self):
-        """test the edit user page works"""
-        url = reverse('admin:core_user_change' , args=[self.user.id])
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-
 
         self.assertEqual(res.status_code, 200)
 
-        
+  
